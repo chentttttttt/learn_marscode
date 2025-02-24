8679e8da8af0305b35a70cc9450633d698e354eb
cctttt <2256472727@qq.com>
2025-02-24 14:41:30 +0800

    fix mode

--- test.py
+++ test.py
@@ -5,8 +5,7 @@ class TestProcessArray(unittest.TestCase):
 
     def test_empty_array(self):
         # 测试空数组
-        arr = []
-        result = process_array(arr)
+        result = process_array([])
         self.assertEqual(result['mean'], 0)
         self.assertIsNone(result['max'])
         self.assertIsNone(result['min'])
@@ -15,8 +14,7 @@ class TestProcessArray(unittest.TestCase):
 
     def test_single_element_array(self):
         # 测试单元素数组
-        arr = [5]
-        result = process_array(arr)
+        result = process_array([5])
         self.assertEqual(result['mean'], 5)
         self.assertEqual(result['max'], 5)
         self.assertEqual(result['min'], 5)
@@ -30,18 +28,28 @@ class TestProcessArray(unittest.TestCase):
         self.assertEqual(result['mean'], 3)
         self.assertEqual(result['max'], 5)
         self.assertEqual(result['min'], 1)
-        self.assertEqual(result['mode'], None)
+        self.assertEqual(result['mode'], [1, 2, 3, 4, 5])
         self.assertEqual(result['median'], 3)
 
-    def test_duplicate_elements_array(self):
-        # 测试有重复元素的数组
-        arr = [1, 2, 2, 3, 3, 3]
+    def test_even_length_array(self):
+        # 测试偶数长度数组
+        arr = [1, 2, 3, 4]
         result = process_array(arr)
-        self.assertEqual(result['mean'], 2)
-        self.assertEqual(result['max'], 3)
+        self.assertEqual(result['mean'], 2.5)
+        self.assertEqual(result['max'], 4)
         self.assertEqual(result['min'], 1)
-        self.assertEqual(result['mode'], 3)
+        self.assertEqual(result['mode'], [1, 2, 3, 4])
         self.assertEqual(result['median'], 2.5)
+
+    def test_odd_length_array(self):
+        # 测试奇数长度数组
+        arr = [1, 2, 3, 4, 5, 6, 7]
+        result = process_array(arr)
+        self.assertEqual(result['mean'], 4)
+        self.assertEqual(result['max'], 7)
+        self.assertEqual(result['min'], 1)
+        self.assertEqual(result['mode'], [1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(result['median'], 4)
 
 if __name__ == '__main__':
     unittest.main()
